21e8998351f0feba9ca2a40aa23c1546bf897352
Avarfox <Libertacoro@yandex.ru>
2025-10-03 17:48:23 +0300

    version 2.1

--- 01/algorithm.h
+++ 01/algorithm.h
@@ -6,3 +6,4 @@ std::string clear_number(const std::string& str);
 std::string trim(const std::string& str);
 std::string convertTextToSound(std::string text);
 bool isEqual(std::string text1, std::string text2);
+char table(char chr);
